d833186cf00b1280ae0fec3ac77826034117e561
Oliver Weiler <oliver.weiler@meinestadt.de>
2019-08-21 18:38:30 +0200

    Read credentials from settings.xml

--- src/main/groovy/com/github/helpermethod/UpdateDependencyMojo.groovy
+++ src/main/groovy/com/github/helpermethod/UpdateDependencyMojo.groovy
@@ -1,6 +1,6 @@
 package com.github.helpermethod
 
-import org.apache.maven.artifact.Artifact
+
 import org.apache.maven.artifact.factory.ArtifactFactory
 import org.apache.maven.artifact.metadata.ArtifactMetadataSource
 import org.apache.maven.artifact.repository.ArtifactRepository
@@ -11,8 +11,15 @@ import org.apache.maven.plugins.annotations.Component
 import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.project.MavenProject
+import org.apache.maven.settings.Settings
 import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.BaseRepositoryBuilder
 import org.eclipse.jgit.lib.PersonIdent
+import org.eclipse.jgit.lib.Repository
+import org.eclipse.jgit.lib.RepositoryBuilder
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder
+import org.eclipse.jgit.transport.URIish
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 
 @Mojo(name = "update-dependency")
 class UpdateDependencyMojo extends AbstractMojo {
@@ -22,6 +29,17 @@ class UpdateDependencyMojo extends AbstractMojo {
     ArtifactRepository localRepository
     @Parameter(defaultValue = '${project.remoteArtifactRepositories}', readonly = true)
     List remoteArtifactRepositories
+
+    @Parameter(property = 'connectionUrl', defaultValue = '${project.scm.connection}')
+    String connectionUrl
+    @Parameter(property = 'developerConnectionUrl', defaultValue = '${project.scm.developerConnection}')
+    String developerConnectionUrl
+    @Parameter(property = "connectionType", defaultValue = "connection")
+    String connectionType
+
+    @Parameter(defaultValue = '${settings}', readonly = true)
+    Settings settings
+
     @Component
     ArtifactMetadataSource artifactMetadataSource
     @Component
@@ -29,7 +47,7 @@ class UpdateDependencyMojo extends AbstractMojo {
 
     void execute() throws MojoExecutionException, MojoFailureException {
         // update parent version
-        Artifact parent = artifactFactory.createParentArtifact(mavenProject.parent.groupId, mavenProject.parent.artifactId, mavenProject.parent.version)
+        def parent = artifactFactory.createParentArtifact(mavenProject.parent.groupId, mavenProject.parent.artifactId, mavenProject.parent.version)
         def versions = artifactMetadataSource.retrieveAvailableVersions(parent, localRepository, remoteArtifactRepositories)
         def latestParentVersion = versions.max().toString()
 
@@ -40,7 +58,7 @@ class UpdateDependencyMojo extends AbstractMojo {
         def git = Git.open(mavenProject.basedir)
         git.checkout().tap {
             createBranch = true
-            name = "continuous-dependency-update/${parent.groupId}-${parent.artifactId}-${parent.version}"
+            name = "continuous-dependency-update/${parent.groupId}-${parent.artifactId}-${latestParentVersion}"
         }.call()
 
         def modifiedPom = new XmlParser(false, false).parse(mavenProject.file).tap {
@@ -51,6 +69,7 @@ class UpdateDependencyMojo extends AbstractMojo {
         mavenProject.file.withPrintWriter {
             new XmlNodePrinter(it, " " * 4).tap {
                 preserveWhitespace = true
+                expandEmptyElements
             }.print(modifiedPom)
         }
 
@@ -60,7 +79,19 @@ class UpdateDependencyMojo extends AbstractMojo {
         // git commit
         git.commit().tap {
             author = new PersonIdent('continuous-dependency-update', '')
-            message = "Bump ${parent.groupId}-${parent.artifactId} from ${parent.version} to $latestParentVersion"
+            message = "Bump ${parent.artifactId} from ${parent.version} to $latestParentVersion"
+        }.call()
+
+        def scm = new URIish(connectionType == 'developerConnection' ? developerConnectionUrl : connectionUrl)
+
+        git.remoteAdd().tap {
+            name = 'origin'
+            uri = scm
+        }.call()
+
+        git.push().tap {
+            def server = settings.getServer(scm.host)
+            credentialsProvider = new UsernamePasswordCredentialsProvider(server.username, server.password)
         }.call()
     }
 }
