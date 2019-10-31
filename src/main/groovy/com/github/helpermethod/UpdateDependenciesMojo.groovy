acde09ebb4c4b8c26385c8a9f55e53e0bff89e69
Oliver Weiler <oliver.weiler@meinestadt.de>
2019-10-31 08:27:41 +0100

    Prepare project for OSSRH

--- src/main/groovy/com/github/helpermethod/UpdateDependenciesMojo.groovy
+++ src/main/groovy/com/github/helpermethod/UpdateDependenciesMojo.groovy
@@ -15,92 +15,100 @@ import org.eclipse.jgit.api.Git
 import org.eclipse.jgit.api.TransportConfigCallback
 import org.eclipse.jgit.lib.PersonIdent
 import org.eclipse.jgit.lib.RepositoryBuilder
-import org.eclipse.jgit.transport.*
+import org.eclipse.jgit.transport.JschConfigSessionFactory
+import org.eclipse.jgit.transport.RefSpec
+import org.eclipse.jgit.transport.Transport
+import org.eclipse.jgit.transport.URIish
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider
 import org.eclipse.jgit.util.FS
 
-import static org.eclipse.jgit.transport.OpenSshConfig.*
+import static org.eclipse.jgit.transport.OpenSshConfig.Host
 
 @Mojo(name = "update-dependencies")
 class UpdateDependenciesMojo extends AbstractMojo {
     @Parameter(defaultValue = '${project}', readonly = true)
     MavenProject mavenProject
-
     @Parameter(defaultValue = '${localRepository}', readonly = true)
     ArtifactRepository localRepository
     @Parameter(defaultValue = '${project.remoteArtifactRepositories}', readonly = true)
     List remoteArtifactRepositories
+    @Parameter(defaultValue = '${settings}', readonly = true)
+    Settings settings
 
     @Parameter(property = 'connectionUrl', defaultValue = '${project.scm.connection}')
     String connectionUrl
     @Parameter(property = 'developerConnectionUrl', defaultValue = '${project.scm.developerConnection}')
     String developerConnectionUrl
-    @Parameter(property = "connectionType", defaultValue = "connection")
+    @Parameter(property = "connectionType", defaultValue = 'connection')
     String connectionType
-
-    @Parameter(defaultValue = '${settings}', readonly = true)
-    Settings settings
+    @Parameter(property = 'allowSnapshots', defaultValue = 'false')
+    boolean allowSnapshots
 
     @Component
     ArtifactMetadataSource artifactMetadataSource
 
     void execute() {
         withGit { git, head ->
-            [this.&parentUpdate].findResults { it() }.each { update ->
-                git
-                    .checkout()
-                    .setStartPoint(head)
-                    .setCreateBranch(true)
-                    .setName("continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}")
-                    .call()
+            ([this.&parentUpdate(), this.&dependencyUpdates()] - null)
+                .flatten()
+                .each { update ->
+                    git
+                        .checkout()
+                        .setStartPoint(head)
+                        .setCreateBranch(true)
+                        .setName("continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}")
+                        .call()
 
-                withPom(update.modification)
+                    withPom(update.modification)
 
-                git
-                    .add()
-                    .addFilepattern('pom.xml')
-                    .call()
+                    git
+                        .add()
+                        .addFilepattern('pom.xml')
+                        .call()
 
-                git
-                    .commit()
-                    .setAuthor(new PersonIdent('continuous-update-bot', ''))
-                    .setMessage("Bump ${update.artifactId} from ${update.currentVersion} to $update.latestVersion")
-                    .call()
+                    git
+                        .commit()
+                        .setAuthor(new PersonIdent('continuous-update-bot', ''))
+                        .setMessage("Bump ${update.artifactId} from ${update.currentVersion} to $update.latestVersion")
+                        .call()
 
-                git
-                    .push()
-                    .setRefSpecs(new RefSpec("continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}:continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}"))
-                    .tap {
-                        def connectionUri = new URIish(connection - 'scm:git:')
+                    git
+                        .push()
+                        .setRefSpecs(new RefSpec("continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}:continuous-dependency-update/${update.groupId}-${update.artifactId}-${update.latestVersion}"))
+                        .tap {
+                            def connectionUri = new URIish(connection - 'scm:git:')
 
-                        switch (connectionUri.scheme) {
-                            case 'https':
-                                def (username, password) = getCredentials(connectionUri)
+                            switch (connectionUri.scheme) {
+                                case 'https':
+                                    def (username, password) = getUsernamePasswordCredentials(connectionUri)
 
-                                setCredentialsProvider(new UsernamePasswordCredentialsProvider(username, password))
+                                    credentialsProvider = new UsernamePasswordCredentialsProvider(username, password)
 
-                                break
-                            default:
-                                setTransportConfigCallback(new TransportConfigCallback() {
-                                    void configure(Transport transport) {
-                                        ((SshTransport)transport).sshSessionFactory = new JschConfigSessionFactory() {
-                                            @Override
-                                            protected JSch createDefaultJSch(FS fs) throws JSchException {
-                                                super.createDefaultJSch(fs).tap {
-                                                    addIdentity('/Users/oliver.weiler/.ssh/id_rsa', 'p0lyfr4g')
+                                    break
+                                default:
+                                    def (privateKey, passPhrase) = getPublicKeyCredentials(connectionUri)
+
+                                    transportConfigCallback = new TransportConfigCallback() {
+                                        void configure(Transport transport) {
+                                            transport.sshSessionFactory = new JschConfigSessionFactory() {
+                                                @Override
+                                                protected JSch createDefaultJSch(FS fs) throws JSchException {
+                                                    super.createDefaultJSch(fs).tap {
+                                                        addIdentity(privateKey, passPhrase)
+                                                    }
                                                 }
-                                            }
 
-                                            protected void configure(Host host, Session session) {
-                                                // NOOP
+                                                protected void configure(Host host, Session session) {
+                                                    // NOOP
+                                                }
                                             }
                                         }
                                     }
-                                })
+                            }
                         }
-                    }
-                    .setPushOptions(['merge_request.create'])
-                    .call()
-            }
+                        .setPushOptions(['merge_request.create'])
+                        .call()
+                }
         }
     }
 
@@ -132,12 +140,16 @@ class UpdateDependenciesMojo extends AbstractMojo {
         connectionType == 'developerConnection' ? developerConnectionUrl : connectionUrl
     }
 
-    private def getCredentials(URIish uri) {
+    private def getUsernamePasswordCredentials(uri) {
         uri.user ? [uri.user, uri.pass] : settings.getServer(uri.host).with { [it.username, it.password] }
     }
 
+    private def getPublicKeyCredentials(uri) {
+        settings.getServer(uri.host).with { [it.privateKey, it.passphrase] }
+    }
+
     private def parentUpdate() {
-        if (!mavenProject.parentArtifact) return null
+        if (!mavenProject.parentArtifact) return []
 
         update(mavenProject.parentArtifact) { version, pom ->
             pom.children().find { it.name() == 'parent' }.version[0].value = version
@@ -145,7 +157,7 @@ class UpdateDependenciesMojo extends AbstractMojo {
     }
 
     private def dependencyUpdates() {
-        mavenProject.dependencyArtifacts.collect { artifact ->
+        mavenProject.dependencyArtifacts.findResults { artifact ->
             update(artifact) { version, pom ->
                 pom.dependencies.dependency.find {
                     it.artifactId == artifact.artifactId && it.groupId == artifact.groupId && it.version == artifact.version
@@ -156,16 +168,16 @@ class UpdateDependenciesMojo extends AbstractMojo {
 
     private def update(artifact, modification) {
         def versions = artifactMetadataSource.retrieveAvailableVersions(artifact, localRepository, remoteArtifactRepositories)
-        def latestVersion = versions.max().toString()
+        def latestVersion = versions.findAll { it.qualifier != 'SNAPSHOT' }.max().toString()
 
         if (artifact.version == latestVersion) return null
 
         [
-            groupId: artifact.groupId,
-            artifactId: artifact.artifactId,
-            currentVersion: mavenProject.parentArtifact.version,
-            latestVersion: latestVersion,
-            modification: modification.curry(latestVersion)
+            groupId       : artifact.groupId,
+            artifactId    : artifact.artifactId,
+            currentVersion: artifact.version,
+            latestVersion : latestVersion,
+            modification  : modification.curry(latestVersion)
         ]
     }
 }
