a4a473845c4660f294e1c11c19c4a5a71f743a5f
Karl Heinz Marbaise <kama@soebes.de>
2021-02-24 19:02:21 +0100

    #9 Excluded dependency to junit 3.X
    Co-Authored-By: Sandra Parsick <sparsick@web.de>
    Co-Authored-By: Georg Berky <georg.berky@posteo.de>

--- src/test/kotlin/com/github/helpermethod/ArtifactVersionComparisonTest.kt
+++ src/test/kotlin/com/github/helpermethod/ArtifactVersionComparisonTest.kt
@@ -1,6 +1,5 @@
 package com.github.helpermethod
 
-import org.apache.maven.artifact.versioning.ArtifactVersion
 import org.apache.maven.artifact.versioning.DefaultArtifactVersion
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
